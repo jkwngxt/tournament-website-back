fa7b64f1d2d19fb0ce3c4680c96d5e32d3e4ced2
KAwfang <jitlada.yt@gmail.com>
2024-10-08 18:31:08 +0700

    initial project

---
+++ src/main/java/ku/th/tournamentwebsiteback/TournamentWebsiteBackApplication.java
@@ -0,0 +1,13 @@
+package ku.th.tournamentwebsiteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentWebsiteBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentWebsiteBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ku/th/tournamentwebsiteback/TournamentWebsiteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.th.tournamentwebsiteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentWebsiteBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
