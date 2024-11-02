fd20b6b4f5f3989e3e4b44da54171f9b42b5624b
KAwfang <jitlada.yt@gmail.com>
2024-11-02 20:09:23 +0700

    refactor api paths in team controller

--- src/main/java/ku/th/tournamentwebsiteback/controller/TeamController.java
+++ src/main/java/ku/th/tournamentwebsiteback/controller/TeamController.java
@@ -2,6 +2,7 @@ package ku.th.tournamentwebsiteback.controller;
 
 import ku.th.tournamentwebsiteback.dto.TeamDetailDTO;
 import ku.th.tournamentwebsiteback.exception.UnauthorizedException;
+import ku.th.tournamentwebsiteback.request.TeamRequest;
 import ku.th.tournamentwebsiteback.request.ValidateTeamRequest;
 import ku.th.tournamentwebsiteback.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,43 +14,49 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/teams")
 public class TeamController {
     @Autowired
     TeamService teamService;
 
-    @GetMapping
+    @GetMapping("/teams")
     public ResponseEntity<List<TeamDetailDTO>> getAllTeams() {
         List<TeamDetailDTO> teams = teamService.getAllTeams();
         return ResponseEntity.ok(teams);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/teams/{id}")
     public ResponseEntity<TeamDetailDTO> getTeamById(@PathVariable UUID id) {
         TeamDetailDTO team = teamService.getTeamById(id);
         return ResponseEntity.ok(team);
     }
 
+    // Create team
+    @PostMapping("/teams")
+    public ResponseEntity<String> createTeam(@RequestBody TeamRequest request) {
+        teamService.createTeam(request);
+        return ResponseEntity.ok("Team created successfully");
+    }
+
+    // Validate team = {rejected/approved}
+    @PutMapping("/teams/{id}/validate")
+    public ResponseEntity<String> validateTeam(@PathVariable UUID id, @RequestBody ValidateTeamRequest request) {
+        teamService.validateTeam(id, request);
+        return ResponseEntity.ok("Team validated successfully");
+    }
+
     // Get all teams in Tournament
-    @GetMapping("/tournament/{id}")
+    @GetMapping("/tournaments/{tournamentId}/teams")
     public ResponseEntity<List<TeamDetailDTO>> getTeamByTournamentId(@PathVariable UUID id) {
         List<TeamDetailDTO> teams = teamService.getTeamByTournamentId(id);
         return ResponseEntity.ok(teams);
     }
 
     // Get team's user in specific tournament
-    @GetMapping("/tournament/{id}/user")
-    public ResponseEntity<TeamDetailDTO> getTeamByTournamentIdAndUserId(@PathVariable UUID id) {
+    @GetMapping("/tournaments/{tournamentId}/teams/me")
+    public ResponseEntity<TeamDetailDTO> getTeamByTournamentIdAndUserId(@PathVariable UUID tournamentId) {
         Integer userId = getCurrentUserId();
-        TeamDetailDTO team = teamService.getTeamByTournamentIdAndUserId(userId, id);
+        TeamDetailDTO team = teamService.getTeamByTournamentIdAndUserId(userId, tournamentId);
         return ResponseEntity.ok(team);
-    }
-
-    // Validate team = {rejected/approved}
-    @PutMapping("/validate/{id}")
-    public ResponseEntity<String> validateTeam(@PathVariable UUID id, @RequestBody ValidateTeamRequest request) {
-        teamService.validateTeam(id, request);
-        return ResponseEntity.ok("Team validated successfully");
     }
 
     private Integer getCurrentUserId() {
